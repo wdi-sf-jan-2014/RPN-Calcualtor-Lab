3cc263d9c68a851e52ba86cab382e06a018a36fd
amandragon <aee8@duke.edu>
2014-03-17 16:05:39 -0700

    stack.rb tests are passing

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -2,10 +2,13 @@ require './stack.rb'
 
 # Do not use a ruby array!  Add your list class instead
 
+
 class RPNCalculator
  
   # Parse should return a list class that you defined, not a ruby array 
   def self.parse(rpn_string)
+    new_list = rpn_string.split
+    return new_list.to_f
   end
 
   def self.evaluate(rpn_list)
--- stack.rb
+++ stack.rb
@@ -1,5 +1,67 @@
 # The stack should be implemented using a linked list.
 # Do not use any ruby arrays!!
 
+class Node
+  attr_accessor :next, :previous, :value
+  def initialize(value)
+    @value = value
+  end
+end
+
 class Stack
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+  end
+
+    attr_accessor :push, :pop, :length
+
+  def push(value)
+      if @head == nil
+        @head = Node.new(value)
+        @tail = @head
+      else
+        old_tail = @tail
+        @tail = Node.new(value)
+        old_tail.next = @tail
+        @tail.previous = old_tail
+      end
+      @length += 1
+      nil
+  end
+
+  def pop
+    return nil if @length == 0
+ 
+    if @length == 1
+      return_value = @tail.value
+      nil_node(@tail)
+      @tail = nil
+      @head = nil
+      @length = 0
+      return return_value
+    end
+
+    old_tail = @tail
+    @tail = old_tail.previous
+    @tail.next = nil
+
+    return_value = old_tail.value
+
+    @length -= 1
+ 
+    nil_node(old_tail)
+
+    return_value
+  end
+
+  private
+  def nil_node(node)
+    node.value = nil
+    node.previous = nil
+    node.next = nil
+  end
+
 end
