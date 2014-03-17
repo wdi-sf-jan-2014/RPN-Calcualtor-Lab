7b7054dbb05372a51fceb2b7e4c4abe6270e8d0e
Eric Perez <ericricardoperez@gmail.com>
2014-03-17 16:08:20 -0700

    created node class, pop, push methods in Stack class

--- stack.rb
+++ stack.rb
@@ -2,4 +2,45 @@
 # Do not use any ruby arrays!!
 
 class Stack
+  attr_reader :length
+  attr_accessor :tail
+
+  def initialize
+    @tail = nil
+    @length = 0
+  end
+
+  def push(value)
+    node = Node.new(value)
+    if @tail.nil?
+      @tail = node
+    else
+      node.previous = @tail
+      @tail = node
+    end
+    @length += 1
+    return self
+  end
+
+  def pop
+    if @tail.nil?
+      return nil
+    else
+      node = @tail
+      @tail = node.previous
+      @length -= 1
+      return node.value
+    end
+  end
+
+end
+
+class Node
+  attr_accessor :value, :previous
+
+  def initialize(value)
+    @value = value
+    @previous = nil
+  end
+
 end
