1810f5d3e9555b96311ada7c2d5067b3f5babd6f
gthetwong <gthetwong@gmail.com>
2014-03-17 17:56:41 -0700

    rpn_calculator almost working

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -7,13 +7,25 @@ class RPNCalculator
   # Parse should return a list class that you defined, not a ruby array 
   def self.parse(rpn_string)
     rpn_string.split(" ")
-    stack = Stack.new()
-    rpn_string.each do |f|
-      stack.push(f)
-    end
   end
 
   def self.evaluate(rpn_list)
+    stack = Stack.new()
+    rpn_list.each do |f|
+      if self.is_operation(f)
+        stack.push(f)
+        operator = stack.pop
+        value2 = (stack.pop).to_f
+        value1 = (stack.pop).to_f
+        return_value = value1.public_send(operator,value2)
+        stack.push(return_value)
+        return return_value
+      else
+        stack.push(f)
+      end
+
+    end
+
 
   end
 
