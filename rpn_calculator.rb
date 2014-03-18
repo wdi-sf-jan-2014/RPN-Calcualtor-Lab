9892fa732a5723b8973dca4954b30e799b8ccd93
cbestiva <cbestiva@gmail.com>
2014-03-18 10:04:14 -0700

    passing all specs

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -6,14 +6,36 @@ class RPNCalculator
  
   # Parse should return a list class that you defined, not a ruby array 
   def self.parse(rpn_string)
+    list = rpn_string.split(" ")
   end
 
   def self.evaluate(rpn_list)
+    new_stack = Stack.new
+    rpn_list.each do |x|
+      if self.is_number(x)
+        new_stack.push(x)
+      elsif self.is_operation(x)
+        if x == '^'
+          operator = '**'
+        else
+          operator = x
+        end
+        val2 = new_stack.pop
+        val1 = new_stack.pop
+        result = val1.to_f.public_send(operator, val2.to_f)
+        new_stack.push(result)
+      end
+    end
+    return new_stack.pop
   end
 
   def self.is_operation(operation)
+    operation.match(/[\*+-\/\^]/) == nil ? false: true
   end
   
   def self.is_number(num)
+    num.match(/\A[+-]?\d+?(_?\d+)*(\.\d+e?\d*)?\Z/) == nil ? false : true
   end
+
+
 end
--- stack.rb
+++ stack.rb
@@ -1,5 +1,59 @@
 # The stack should be implemented using a linked list.
 # Do not use any ruby arrays!!
 
+class Node
+  attr_accessor :next, :previous, :value
+  
+  def initialize(value)
+    @value = value
+  end
+end
+
 class Stack
+
+  attr_reader :length
+  attr_accessor :head, :tail
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+  end
+
+  # Adds a new value to the end of the list.
+  def push(value)
+    newNode = Node.new(value)
+    if @head.nil?
+      @head = newNode
+      @tail = @head
+    else 
+      old_tail = @tail
+      @tail = newNode
+      @tail.previous = old_tail
+    end
+    @length += 1
+    nil
+  end
+
+  # Returns the value that is popped off
+  # or nil if none exists
+  def pop
+    # return nil if @length == 0
+    # Tim did a fancy private method.....
+    if @tail.nil?
+      old_tail = nil
+    else
+      old_tail = @tail.value
+      if @tail.previous.nil?
+        @tail = nil
+      else
+        new_tail = @tail.previous
+        @tail = new_tail
+      end
+      @length -= 1
+    end
+    # @tail.previous = new_tail.previous  
+    return old_tail
+
+  end
 end
