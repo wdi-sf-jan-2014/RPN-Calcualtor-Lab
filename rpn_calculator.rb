6f8cf16a8e0c607a78f1f9eb3e516cfcce682f01
melodicodyssey <stevedbauer@gmail.com>
2014-03-17 22:44:49 -0700

    Had to fix every method due to misconception about program.  All RSPEC tests passing now

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -1,4 +1,5 @@
 require './stack.rb'
+require 'pry'
 
 # Do not use a ruby array!  Add your list class instead
 
@@ -6,24 +7,33 @@ class RPNCalculator
  
   # Parse should return a list class that you defined, not a ruby array 
   def self.parse(rpn_string)
-    rpn_list = Stack.new
-    array = rpn_string.split(" ")
-    array.each do |el|
-      rpn_list.push(el)
-    end
-    return rpn_list
+    rpn_string.split(" ")
   end
 
   def self.evaluate(rpn_list)
-    
+    stack = Stack.new
+
+    rpn_list.each do |i|
+      if RPNCalculator.is_number(i)
+        stack.push(i).to_f
+      end
+      if RPNCalculator.is_operation(i)
+        y = stack.pop.to_f
+        x = stack.pop.to_f
+        stack.push((x).method(i).(y))
+      end
+    end
+
+    return stack.pop
+
   end
 
   def self.is_operation(operation)
-    # if operation.match(/[\/+-\\*]/) then return true else return false end
-    if operation.match(/[^\d\w\s"]/) then return true else return false end
+    if operation.match(/[^\d\w\s"=]/) then return true else return false end
   end
   
   def self.is_number(num)
     if num.match(/[\d]+/) then return true else return false end
   end
+
 end
