fb311233b5b27ca497b00273c5d184b27bd65518
mayormcmatt <matthew.is.riding@gmail.com>
2014-03-18 10:20:25 -0700

    Adaptation from Steve M work and things found online but not complete

--- stack.rb
+++ stack.rb
@@ -2,4 +2,32 @@
 # Do not use any ruby arrays!!
 
 class Stack
+    def initialize
+    @list = List.new
+  end
+
+  def length
+    @list.length
+  end
+
+  # Adds a new value to the end/top of the stack
+  def push(value)
+    @list.push(value)
+  end
+
+  # Returns the value that is popped off top of stack
+  # or nil if none exists
+  def pop
+    @list.pop
+  end
+
+  def show
+    puts('Top')
+    @list.show_rev
+    puts('Bottom')
+  end
+
+  def peak
+    return @list.last
+  end
 end
