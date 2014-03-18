fb6a57a1e6a3a93ac6fc28436c8eb5e361fd3bab
afleisch <afleisch@gmail.com>
2014-03-17 20:42:29 -0700

    parse and operation tests passing

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -6,14 +6,19 @@ class RPNCalculator
  
   # Parse should return a list class that you defined, not a ruby array 
   def self.parse(rpn_string)
+    rpn_string.split(" ")
   end
 
   def self.evaluate(rpn_list)
+    rpn_list
   end
 
   def self.is_operation(operation)
+    operations = ["+", "-", "*", "/", "^"]
+    operations.include?(operation)
   end
   
   def self.is_number(num)
+    num.to_i =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
   end
 end
