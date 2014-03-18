ae2c26b9539eac76eefabf33b72be50699eba2dc
afleisch <afleisch@gmail.com>
2014-03-17 21:53:44 -0700

    is_number test passing

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -11,7 +11,17 @@ class RPNCalculator
   end
 
   def self.evaluate(rpn_list)
-    rpn_list
+    stack = Stack.new
+    rpn_list.each do |x|
+      if self.is_number(x)
+        stack.push(x.to_f)
+      elsif self.is_operation(x)
+        operator = x
+        num1 = stack.pop
+        num2 = stack.pop
+        value = num2.public_send(operator, num1)
+        stack.push(value)
+      end
   end
 
   def self.is_operation(operation)
