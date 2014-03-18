b93243015e52e9bf7af254d3ff0bb2e095932571
afleisch <afleisch@gmail.com>
2014-03-17 22:13:23 -0700

    stack tests passing

--- stack.rb
+++ stack.rb
@@ -1,5 +1,58 @@
 # The stack should be implemented using a linked list.
 # Do not use any ruby arrays!!
+require './node.rb'
 
 class Stack
+  attr_reader :length
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+  end
+
+
+  def push(value)
+    if @head == nil
+      @head = Node.new(value)
+      @tail = @head
+    else
+      old_tail = @tail
+      @tail = Node.new(value)
+      old_tail.next = @tail
+      @tail.previous = old_tail
+    end
+    @length += 1
+    nil
+  end
+
+  def pop
+    return nil if @length == 0
+
+    # special case where the length of the list is 1
+    # so the head and tail need to be set to nil
+    if @length == 1
+      return_value = @tail.value
+      @tail = nil
+      @head = nil
+      @length = 0
+      return return_value
+    end
+
+    old_tail = @tail
+    @tail = old_tail.previous
+    @tail.next = nil
+
+    return_value = old_tail.value
+
+    @length -= 1
+
+    # Making sure the old_tail has no references to nodes or
+    # values.
+    
+
+    return_value
+  end
 end
+
+
